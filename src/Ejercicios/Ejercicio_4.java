5ffebcf7fa9ee3c0b6b3225f02741d88db70671e
Alicia <alicia.tarifa.8@gmail.com>
2022-04-18 22:12:46 -0300

    Ejercicio 4

---
+++ src/Ejercicios/Ejercicio_4.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_4 {
+
+	/*
+	 * Declarar dos variables, asignar un valor cualquiera y realizar las siguientes operaciones:
+	 * Mostrar la suma
+	 * Mostrar la multiplicacion
+	 * Mostrar la resta
+	 * Mostrar Division
+	 */
+	public static void main(String[] args) {
+		
+		double valor1 = 35.6;
+		double valor2 = 12.5;
+		
+		double suma = valor1 + valor2;
+		System.out.println(valor1 + " + " + valor2 + " = " + suma);
+		
+		double multiplicacion = valor1 * valor2;
+		System.out.println(valor1 + " * " + valor2 + " = " + multiplicacion);
+		
+		double resta = valor1 - valor2;
+		System.out.println(valor1 + " - " + valor2 + " = " + resta);
+		
+		double division = valor1 / valor2;
+		System.out.println(valor1 + " / " + valor2 + " = " + division);
+		
+		
+		
+		
+
+	}
+
+}
