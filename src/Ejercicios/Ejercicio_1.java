1ea45f960f14b73bb4a1855541c828dc31f76017
Alicia <alicia.tarifa.8@gmail.com>
2022-04-18 21:04:02 -0300

    Ejercicio 2

---
+++ src/Ejercicios/Ejercicio_1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_1 {
+
+	/*
+	 * 	Declarar una variable de tipo cadena (String) y una variable de tipo entero, asignar a la
+	 * variable string tu nombre y a la variable entera tu edad y mostrar el mensaje "Mi nombre
+	 * es...y mi edad es..."
+	 */
+	public static void main(String[] args) {
+		
+		String nombre = "Alicia Tarifa";
+		int edad = 35;
+		
+		System.out.println("Mi nombre es " + nombre + " y mi edad es "+ edad + " años");
+
+	}
+
+}
---
+++ src/Ejercicios/Ejercicio_2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_2 {
+
+	/*
+	 * Declarar dos variables de tipo entero, asignar un valor y mostrarla por pantalla.
+	 */
+	public static void main(String[] args) {
+		
+		int num1 = 356;
+		int num2 = 789;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+
+	}
+
+}
