cd9285843dff3cc6e3bb485d185d601ab65d3a66
Alicia <alicia.tarifa.8@gmail.com>
2022-04-19 12:56:53 -0300

    Ejercicio 4

---
+++ src/Ejercicios/Ejercicio_5.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_5 {
+
+	/*
+	 * Declare un variable de tipo entero y asigne un valor cualquiera, determine si el numero es
+	 * par o impar y muestre un mensaje con esa informacion. mensaje: "El numero... es (par o
+	 * impar)"
+	 */
+	
+	public static void main(String[] args) {
+		
+		int num;
+		num = 955;
+		
+		if (num%2==0) {
+			System.out.println("El número " +num+ " es par");
+		}else {
+			System.out.println("El número " +num+ " es impar");
+		}
+
+	}
+
+}
