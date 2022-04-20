ba6ab2ec9d760a3d199f0094adf63249c1699fb0
Alicia <alicia.tarifa.8@gmail.com>
2022-04-20 19:31:57 -0300

    Ejercicio 8

---
+++ src/Ejercicios/Ejercicio_8.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_8 {
+
+	/*
+	 * Calcular la cantidad de minutos que hay existen en una cantidad de segundos ingresada
+	 * por el usuario.
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int segundosUsuario;
+		int minutos;
+		int segundos;
+		
+		System.out.print("Ingrese cantidad de segundos: ");
+		segundosUsuario = scanner.nextInt();
+		minutos = segundosUsuario / 60;
+		segundos = segundosUsuario % 60;
+		
+		System.out.println(segundosUsuario + " segundos equivalen a " + minutos + " minutos con " + segundos + " segundos");	
+	}
+
+}
