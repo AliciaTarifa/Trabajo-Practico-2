70862f40ec97c1d3f51a0cdd5fe460edeef5b505
Alicia <alicia.tarifa.8@gmail.com>
2022-04-20 18:18:06 -0300

    Ejercicio 6

---
+++ src/Ejercicios/Ejercicio_6.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_6 {
+
+	/*
+	 * Declare dos variables de tipo §otante, asigne un valor a cada una, calcule el promedio 
+	 * y muestrelo por consola.
+	 */
+	public static void main(String[] args) {
+		
+		float numero1;
+		float numero2;
+		
+		numero1 = 30.17f;
+		numero2 = 20.03f;
+		
+		System.out.println("El promedio de: " + numero1 + " y " + numero2 + " es " + (numero1 + numero2) /2);
+
+	}
+
+}
