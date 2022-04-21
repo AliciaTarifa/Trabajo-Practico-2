a1be5c022db230f3b0511b1c39c9bd6aafe9bb8c
Alicia <alicia.tarifa.8@gmail.com>
2022-04-21 15:46:57 -0300

    Ejercicio 14

---
+++ src/Ejercicios/Ejercicio_14.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+import java.util.Random;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_14 {
+
+	/*
+	 *Dado un numero natural N se desea conocer sus divisores y la suma de todos ellos. No
+	 *considerar el 1 si mismo 
+	 */
+	public static void main(String[] args) {
+		
+		int n;
+		int acumulador = 0;
+		int divisor = 2;
+		
+		n = new Random().nextInt(50);
+		System.out.println("Numero: " + n);
+		
+		while(divisor<=n/2) {
+			if(n%divisor==0) {
+				System.out.println("Divisor: " + divisor);
+				acumulador = acumulador+divisor;
+			}
+			
+			divisor++;
+		}
+
+	}
+
+}
