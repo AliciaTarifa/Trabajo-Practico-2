86497633ff1ce763f9b4f473152b683b99d89e2f
Alicia <alicia.tarifa.8@gmail.com>
2022-04-21 11:00:57 -0300

    Ejercicio 10

---
+++ src/Ejercicios/Ejercicio_10.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_10 {
+
+	/*
+	 * Declare dos variables base y altura,que representan la base y la altura de un triangulo.
+	 * Asigne un valor cualquiera a cada una y muestre la super¦cie del mismo.
+	 */
+	public static void main(String[] args) {
+		
+		int base;
+		int altura;
+		
+		base = 65;
+		altura = 22;
+		System.out.println("La superficie del triángulo de base " + base + " y altura " + altura + " es igual a " + (base*altura)/2);
+
+	}
+
+}
